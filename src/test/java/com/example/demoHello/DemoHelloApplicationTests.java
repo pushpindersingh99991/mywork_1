8747db34f55f6100988e63ab8d40a383a1f9ff1b
unknown <pushpinder9488@gmail.com>
2023-06-26 00:50:37 +0530

    my

---
+++ src/test/java/com/example/demoHello/DemoHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
